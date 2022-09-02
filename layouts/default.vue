6fa605aa717605ea537b6e2529fe18e1fa6566d3
Abdoulaye Gning <abdoulaye@lapetitetribu.co>
2022-09-02 17:05:48 +0200

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        
+    },
+
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
